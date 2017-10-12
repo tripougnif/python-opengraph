443d5c33c00b1a71f3479852d336237d06deaf77
Jason Robinson <mail@jasonrobinson.me>
2017-10-12 22:06:57 +0300

    Set version in package

--- opengraph/__init__.py
+++ opengraph/__init__.py
@@ -1,1 +1,2 @@
+from .__version__ import __version__  # NOQA
 from .opengraph import OpenGraph  # NOQA
--- setup.py
+++ setup.py
@@ -1,28 +1,36 @@
+import re
 from setuptools import setup, find_packages
 
-VERSION = (0, 0, 3)
-__version__ = VERSION
-__versionstr__ = '.'.join([str(v) for v in VERSION])
+install_requires = ["beautifulsoup4>=4.3", "requests>=2.7"]
 
-install_requires = ['beautifulsoup4>=4.3', 'requests>=2.7']
+# Get version
+# Tip fromhttps://stackoverflow.com/a/7071358/1489738
+with open("opengraph/__version__.py") as version_file:
+    version_string = version_file.read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, version_string, re.M)
+if mo:
+    __version__ = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string.")
 
 setup(
-    name='python-opengraph',
-    version=__versionstr__,
-    description='Python module to parse Open Graph metadata on web pages',
-    url='https://github.com/jaywink/python-opengraph',
-    license='MIT',
-    author='Jason Robinson',
-    author_email='mail@jasonrobinson.me',
+    name="python-opengraph",
+    version=__version__,
+    description="Python module to parse Open Graph metadata on web pages",
+    url="https://github.com/jaywink/python-opengraph",
+    license="MIT",
+    author="Jason Robinson",
+    author_email="mail@jasonrobinson.me",
     install_requires=install_requires,
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(exclude=["tests"]),
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
-    keywords='opengraph')
+    keywords="opengraph")
